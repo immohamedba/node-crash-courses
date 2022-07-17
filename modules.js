63d65a60bd46ba88dd3db29785cdc2e10020c0fb
BaMohamed <ba.mhd778@gmail.com>
2022-07-17 17:18:49 +0100

    requestes and responses

--- modules.js
+++
@@ -1,6 +0,0 @@
-const {peoples, ages} = require('./people')
-///console.log(peoples, ages);
-
-const os = require('os');
-console.log(os.platform(), os.homedir());
-
--- people.js
+++
@@ -1,8 +0,0 @@
-const people =['yoshi', 'ryu', 'chun-li', 'mario'];
-const ages =[20, 25, 30, 35];
-
-
-module.exports={
-    peoples: people,
-     ages: ages
-}
